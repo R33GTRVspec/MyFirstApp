20533621923bd34c0af5ac588afe5b9bcce416ef
R33GTRVspec <kocmohabt@me.com>
2018-08-06 17:21:23 +0300

    game with date pipes

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -10,6 +10,13 @@ export class CarsComponent {
   carName = '';
   addCarStatus = false;
   cars = ['Ford', 'Audi', 'BMW', 'SEAT', 'Mazda', 'Lada', 'Bentley'];
+  dates = [
+    new Date(2015,3,4),
+    new Date(2011,2,5),
+    new Date(2009,1,14),
+    new Date(2005,6,23),
+  ];
+
 
   constructor() {
   }
@@ -18,9 +25,6 @@ export class CarsComponent {
     this.addCarStatus = true;
     this.cars.push(this.carName);
     this.carName = '';
-  }
-  setBigCarText (car: string) {
-    return car.length > 4 ? true : false;
   }
 
 
